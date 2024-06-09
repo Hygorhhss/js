1ff06f78eedf95be1af440441bf3dea79f883c48
Hygor Henrique <101516308+Hygorhhss@users.noreply.github.com>
2024-06-09 19:56:46 -0300

    atualização

---
+++ src/moduloD/aula1/ex.js
@@ -0,0 +1,7 @@
+var vel =70 //valor real
+console.log('A velocidade do seu carro é '+vel+ 'km/h')
+if(vel >60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida, está multado')
+}
+console.log('Dirija com segurança')
+
